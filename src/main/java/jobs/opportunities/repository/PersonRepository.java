15cbec0c535af80edfcd683f54d2b1c3e949881f
tlachy <tlachy@gmail.com>
2014-11-11 08:14:43 +0100

    delete method hidden

--- src/main/java/jobs/opportunities/repository/PersonRepository.java
+++ src/main/java/jobs/opportunities/repository/PersonRepository.java
@@ -2,10 +2,19 @@ package jobs.opportunities.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import jobs.opportunities.entity.Person;
 
 @RepositoryRestResource(collectionResourceRel = "person", path = "person")
 public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+	@Override
+	@RestResource(exported = false)
+	void delete(Long id);
+
+	@Override
+	@RestResource(exported = false)
+	void delete(Person entity);
 
 }
